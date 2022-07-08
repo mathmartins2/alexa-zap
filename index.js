8d35cf305b955105b73d9b2dd2beb62efad0d7bb
mathmartins2 <dev.sincroni@gmail.com>
2022-07-07 22:13:35 -0300

    v9

--- index.js
+++ index.js
@@ -9,9 +9,19 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const chromiumArgs = [
+    '--disable-web-security', '--no-sandbox', '--disable-web-security',
+    '--aggressive-cache-discard', '--disable-cache', '--disable-application-cache',
+    '--disable-offline-load-stale-cache', '--disk-cache-size=0',
+    '--disable-background-networking', '--disable-default-apps', '--disable-extensions',
+    '--disable-sync', '--disable-translate', '--hide-scrollbars', '--metrics-recording-only',
+    '--mute-audio', '--no-first-run', '--safebrowsing-disable-auto-update',
+    '--ignore-certificate-errors', '--ignore-ssl-errors', '--ignore-certificate-errors-spki-list'
+  ];
+
 create({
     session: 'alexa',
-    headless: false
+    browserArgs: chromiumArgs,
   })
   .then((client) => start(client))
   .catch((erro) => {
