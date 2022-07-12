07a3498cd1b68c9e4bbcdee08da558d9e54704f7
mathmartins2 <dev.sincroni@gmail.com>
2022-07-11 23:21:51 -0300

    v12

--- index.js
+++ index.js
@@ -1,71 +1,54 @@
-import 'dotenv/config';
-import express from 'express';
-import bodyParser from 'body-parser';
-import { create } from 'venom-bot';
+const express = require('express');
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-const chromiumArgs = [
-    '--disable-web-security', '--no-sandbox', '--disable-web-security',
-    '--aggressive-cache-discard', '--disable-cache', '--disable-application-cache',
-    '--disable-offline-load-stale-cache', '--disk-cache-size=0',
-    '--disable-background-networking', '--disable-default-apps', '--disable-extensions',
-    '--disable-sync', '--disable-translate', '--hide-scrollbars', '--metrics-recording-only',
-    '--mute-audio', '--no-first-run', '--safebrowsing-disable-auto-update',
-    '--ignore-certificate-errors', '--ignore-ssl-errors', '--ignore-certificate-errors-spki-list'
-  ];
+const PORT = process.env.PORT || 3000;
 
-create({
-    session: 'alexa',
-    //browserArgs: chromiumArgs,
-    //useChrome: false
-  })
-  .then((client) => start(client))
-  .catch((erro) => {
-    console.log(erro);
+const client = new Client({
+    authStrategy: new LocalAuth({ clientId: "alexa" })
 });
 
-app.get('/', (req, res) => {
-    return res.send('Hello World!');
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
 });
 
-let contacts_ = [];
+client.on('ready', () => {
+    app.get('/sendMessage', async (req, res) => {
+        try {
+            const {from, message} = req.body;
 
-const start = async (client) => {
-    app.post('/sendMessage', async (req, res) => {
-        const { message, from } = req.body;
-        let contact_;
+            if(!from || !message) {
+                return res.status(400).send('Missing parameters');
+            }
         
-        if(!message || !from) {
+            let contacts = await client.getContacts();
+            contacts = contacts.filter((contact) => contact.name === from && contact.isUser === true);
+
+            if(contacts.length === 0) {
+                return res.status(400).json({ error: 'Contact not found' });
+            }
+    
+            await client.sendMessage(contacts[0].id._serialized, message);
+
+            return res.status(200).json({
+                message: 'Message sent'
+            });
+            
+        } catch (error) {
             return res.status(400).json({
-                error: 'Missing parameters'
+                error
             });
         }
-
-        try {
-            if(contacts_.length === 0) {
-                const contacts = await client.getAllContacts();
-                contacts_.push(contacts);
-            }
-
-            contact_ = contacts_.filter((contact) => contact.name === from);
-
-            if (contact_.length === 0) {
-                return res.status(400).json({ error: 'Contact not found' });
-            } 
-
-            await client.sendText(contact_[0].id._serialized, message);
-            return res.status(200).json({message: 'Message sent'});
-        } catch (error) {
-            return res.status(500).json({message: 'Error sending message', error});
-        }
     });
 
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`);
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
-}
+});
+
+client.initialize();
+
